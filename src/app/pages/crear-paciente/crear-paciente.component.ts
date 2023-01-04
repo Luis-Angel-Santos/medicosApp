d4a9e7e34fb6ef837b0a451c07e054b1d5e4756d
Luis Angel Santos <santosangel748@gmail.com>
2023-01-04 13:59:31 -0600

    Mostrando alertas de confirmacion

--- src/app/pages/crear-paciente/crear-paciente.component.ts
+++ src/app/pages/crear-paciente/crear-paciente.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PacientesService } from '../../services/pacientes.service';
 import { Pacientes } from '../../interfaces/pacientes.interface';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-crear-paciente',
@@ -17,14 +19,30 @@ export class CrearPacienteComponent {
     dirpaciente: ''
   };
 
-  constructor(public pacientesService: PacientesService){}
+  constructor(public pacientesService: PacientesService,
+              public router: Router){}
 
   altaPaciente(){
-    this.pacientesService.altaPaciente(this.paciente)
-      .subscribe({
-          next: resul => { console.log(resul); },
-          error: err => { console.log(err); }
-      });
+    Swal.fire({
+      title: '¿Registrar paciente?',
+      showCancelButton: true,
+      confirmButtonText: 'Registrar',
+      confirmButtonColor: 'green',
+      cancelButtonColor: 'red',
+      cancelButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.pacientesService.altaPaciente(this.paciente)
+          .subscribe({
+              next: resul => { this.router.navigate(['/dashboard/nuevo-historial']); },
+              error: err => { console.log(err); }
+          });
+        Swal.fire('Registrado', 'Paciente registrado correctamente', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('Opps', 'Parece que hubo un problema', 'error')
+      }
+    })
+    
   }
 
 }
