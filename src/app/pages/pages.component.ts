58180cc02c713477e64e621e6a88d5680e282108
Luis Angel Santos <santosangel748@gmail.com>
2023-01-03 18:25:08 -0600

    Interfaz de login y dashboard basicas

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PacientesService } from '../services/pacientes.service';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.css']
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit{
+
+  logout(){
+    this.router.navigate(['login']);
+  }
+
+  constructor(public pacientesService: PacientesService,
+              private router: Router){
+    pacientesService.menu;
+  }
+
+  ngOnInit(): void {
+  
+  }
 
 }
--- src/app/services/pacientes.service.ts
+++ src/app/services/pacientes.service.ts
@@ -5,5 +5,31 @@ import { Injectable } from '@angular/core';
 })
 export class PacientesService {
 
+  public menu = [
+    {
+      titulo: 'Doctores',
+      icono: 'fa-solid fa-user-doctor',
+      submenu: [
+        {titulo: 'Datos del Doctor', url: '/dashboard/datos-doctor'}
+      ]
+    },
+    {
+      titulo: 'Pacientes',
+      icono: 'fa fa-user-o',
+      submenu: [
+        {titulo: 'Registrar Paciente', url: '/dashboard/crear-paciente'},
+        {titulo: 'Datos de Pacientes', url: '/dashboard/datos-paciente'}
+      ]
+    },
+    {
+      titulo: 'Historial Clinico',
+      icono: 'fa-solid fa-file-lines',
+      submenu: [
+        {titulo: 'Nuevo Historial', url: '/dashboard/nuevo-historial'},
+        {titulo: 'Historial', url: '/dashboard/historial-paciente'}
+      ]
+    }
+  ]
+
   constructor() { }
 }
